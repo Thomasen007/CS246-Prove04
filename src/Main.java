ff548be7079e586563e1280fb882294019132689
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2021-10-09 16:52:30 -0700

    Thomas's Commit 1.1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello, My name is Krysta");
         System.out.println("Hello Krysta, my name is Thomas!");
+        System.out.println("This is another attemp!");
     }
 }
