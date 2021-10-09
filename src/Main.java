94438c18509ce0af46b036d81050e5c8a68be637
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2021-10-09 16:17:14 -0700

    Thomas's Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello, My name is Krysta");
+        System.out.println("Hello Krysta, my name is Thomas!");
 
     }
 }
